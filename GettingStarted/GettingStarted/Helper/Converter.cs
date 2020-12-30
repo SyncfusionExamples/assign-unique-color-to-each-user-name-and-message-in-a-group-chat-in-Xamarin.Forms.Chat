e581936c24089e061bb82d40f1586e8905f5e6fc
Chandra Sekar <chandrasekar.sampathkumar@syncfusion.com>
2020-12-30 19:29:08 +0530

    Added sample

---
+++ GettingStarted/GettingStarted/Helper/Converter.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.XForms.Chat;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GettingStarted
+{
+    public class AuthorColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var viewModel = parameter as ViewModel;
+            var message = value as IMessage;
+
+            if (viewModel.AuthorColors.ContainsKey(message.Author))
+            {
+                Color color;
+                viewModel.AuthorColors.TryGetValue(message.Author, out color);
+                return color;
+            }
+            else
+            {
+                return viewModel.AddColorForAuthor(message.Author);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Color.White;
+        }
+    }
+}
---
+++ GettingStarted/GettingStarted/Views/AuthorNameColor.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Syncfusion.XForms.Chat;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GettingStarted
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthorColor : ContentPage
+    {
+        public AuthorColor()
+        {
+            InitializeComponent();
+        }
+    }
+}
